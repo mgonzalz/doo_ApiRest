b1a96d55bb3388458ee83b843a7424589a24fefc
mgonzalz <mgonzgarc@myuax.com>
2024-12-01 18:05:08 +0100

    configuraciones del superusuario

--- server/authentication/admin.py
+++ server/authentication/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
 # Register your models here.
+@admin.register(get_user_model()) # Crear, editar y boorar usuarios.
+class CustomUserAdmin(UserAdmin):
+    pass
+
+admin.site.unregister(Group) # Deshabilita los grupos (no se van a usar).
