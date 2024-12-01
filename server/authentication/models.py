e83220c529a44830d37664959c280d3356bf9944
mgonzalz <mgonzgarc@myuax.com>
2024-12-01 17:45:13 +0100

    modificación campos creación de superuser

--- server/authentication/models.py
+++ server/authentication/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=150, unique=True)
+    USERNAME_FIELD = 'email' # Iniciar sesión con el correo, no con el username.
+    REQUIRED_FIELDS = ['username', 'password'] # Campos requeridos para crear un usuario.
--- server/server/settings.py
+++ server/server/settings.py
@@ -88,6 +88,10 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
+# Custom user model
+AUTH_USER_MODEL = "authentication.CustomUser"
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
