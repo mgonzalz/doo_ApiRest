c645719f4a842732370c6fc20e0ea631d0814acc
mgonzalz <mgonzgarc@myuax.com>
2024-12-01 17:32:21 +0100

    Django Rest Framework

--- server/server/settings.py
+++ server/server/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework', # App de RESTful API.
+
     'authentication', # App de autenticación.
 ]
 
